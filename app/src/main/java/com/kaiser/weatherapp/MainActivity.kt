d519856372490fc5ce70917ba7cc8e37378530fc
Kaio Rauta <kaio_soares_rauta@yahoo.com.br>
2020-05-23 22:34:45 -0300

    First commit, working version
    Successfully implemented GPS use for weather forecasting.
    MainActivity in working development state

--- app/src/main/java/com/kaiser/weatherapp/MainActivity.kt
+++ app/src/main/java/com/kaiser/weatherapp/MainActivity.kt
@@ -1,18 +1,221 @@
 package com.kaiser.weatherapp
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.Manifest.*
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.location.Location
+import android.os.Looper
+import android.provider.Settings
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 import com.kaiser.weatherapp.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.net.URL
+import java.util.*
+import kotlin.Exception
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    var LAT: String = ""
+    var LON: String = ""
+    var scope = CoroutineScope(Job() + Dispatchers.IO)
+
+    val City: String = "3447258"
+    val API: String = "ba60cd5d73ef4117b95a302db8d49907"
+    val PERMISSION_ID = 7
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        binding.about.setOnClickListener {
+            requestNewLocationData()
+            WeatherTask().execute() }
+
         setContentView(binding.root)
+
+        getLastLocation()
     }
 
+    private inner class WeatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            /* Showing the ProgressBar, Making the main design GONE */
+            binding.loader.visibility = View.VISIBLE
+            binding.mainContainer.visibility = View.GONE
+            binding.errorText.visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response: String?
+
+            try {
+                response =
+                    URL("https://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON&appid=$API&units=metric").readText(
+                        Charsets.UTF_8
+                    )
+            } catch (e: Exception) {
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            try {
+                /*Extracting JSON returns from the API*/
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val wind = jsonObj.getJSONObject("wind")
+                val sys = jsonObj.getJSONObject("sys")
+
+                val updatedAt = jsonObj.getLong("dt")
+                val updatedAtText = "Updated at: " + formatTicksToDate(updatedAt, "dd/MM/yyyy hh:mm a",Locale.ENGLISH)
+                val temp = main.getDouble("temp").toInt().toString() + "ºC"
+                val tempMin = "Min Temp: " + main.getDouble("temp_min").toInt().toString() + "ºC"
+                val tempMax = "Max Temp: " + main.getDouble("temp_max").toInt().toString() + "ºC"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+
+                val sunrise: Long = sys.getLong("sunrise")
+                val sunset: Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+
+                val address = jsonObj.getString("name") + ", " + sys.getString("country")
+
+                /* Populating extracted data */
+                binding.address.text = address
+                binding.updatedAt.text = updatedAtText
+                binding.status.text = weatherDescription.capitalize()
+                binding.temp.text = temp
+                binding.tempMin.text = tempMin
+                binding.tempMax.text = tempMax
+                binding.sunset.text = formatTicksToDate(sunset, "hh:mm a",Locale.ENGLISH)
+                binding.sunrise.text = formatTicksToDate(sunrise, "hh:mm a", Locale.ENGLISH)
+                binding.wind.text = windSpeed
+                binding.pressure.text = pressure
+                binding.humidity.text = humidity
+
+                /* Views populated, Hiding loader, showing main design */
+                binding.loader.visibility = View.GONE
+                binding.mainContainer.visibility = View.VISIBLE
+
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
+
+    fun formatTicksToDate(timeTicks: Long, format: String, locale: Locale) : String {
+        return SimpleDateFormat(format, locale).format(
+            Date(timeTicks * 1000))
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        LAT = location.latitude.toString()
+                        LON = location.longitude.toString()
+
+                        WeatherTask().execute()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallBack,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallBack = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            LAT = mLastLocation.latitude.toString()
+            LON = mLastLocation.longitude.toString()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this@MainActivity,
+                permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this@MainActivity,
+                permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(permission.ACCESS_COARSE_LOCATION, permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+        }
+    }
 }
